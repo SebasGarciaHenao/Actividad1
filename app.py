6d75d379d8fd0ab00f6ddd40ece98bec7d081273
sebas <sebas084garciahenao@gmail.com>
2023-08-02 14:55:16 -0500

    tercer punto

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ ejercicios.actividad_2.area_circulo
 
 
 print("--- Ejercicio 3 ---")
-import ejercicios.actvidad_3
-ejercicios.actvidad_3.lista_aleatoria
+import ejercicios.actividad_3
+ejercicios.actividad_3.lista_aleatoria
 
-print("--- Ejercicio 4 ---")
-import ejercicios.actividad_4
-ejercicios.actividad_4.par_impar
