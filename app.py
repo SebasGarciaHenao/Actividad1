c5e454baba9aa2d2ce78dcdd3f10b7d2be148ea3
sebas <sebas084garciahenao@gmail.com>
2023-08-02 12:03:21 -0500

    Ejercicios

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
+print("--- Ejercicio 1 ---")
+import ejercicios.actividad_1
+ejercicios.actividad_1.nombre_edad
+
+print("--- Ejercicio 2 ---")
 import ejercicios.actividad_2
 ejercicios.actividad_2.area_circulo
+
+
+
